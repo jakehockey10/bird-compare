ab3a0451dfc73c3c1db89670efe362802c9699d9
Your Name <Tahani@Tahani-Almanies-MacBook-Pro.local>
2015-04-09 16:22:32 -0600

    got a working map

--- public/javascripts/Leaflet/map.js
+++ public/javascripts/Leaflet/map.js
@@ -1,7 +1,7 @@
 
 var map = L.map('map').setView([51.505, -0.09], 13);
-//
-L.tileLayer('https://api.tiles.mapbox.com/v4/access_token=sk.eyJ1IjoidGFoYW5pIiwiYSI6ImkzZzlHaE0ifQ.oUpKTblIElF_hDsUKN6Lqw', {
+// We can also use this 'http://{s}.tile.osm.org/{z}/{x}/{y}.png' without the need of my token. It gives OpenStreetMap tile layer
+L.tileLayer('http://api.tiles.mapbox.com/v4/examples.map-zr0njcqy/{z}/{x}/{y}.png?access_token=sk.eyJ1IjoidGFoYW5pIiwiYSI6ImkzZzlHaE0ifQ.oUpKTblIElF_hDsUKN6Lqw', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18
 }).addTo(map);
@@ -41,4 +41,6 @@ function onMapClick(e) {
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(map);
 }
+
 map.on('click', onMapClick);
+
