dc390b0bfa8066d20ae885a95ff663ecff9f61f8
Your Name <Tahani@engr2-22-2-dhcp.int.colorado.edu>
2015-04-08 18:41:25 -0600

    Leaflet library and map test page

---
+++ Leaflet/map.js
@@ -0,0 +1,44 @@
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+//
+L.tileLayer('https://api.tiles.mapbox.com/v4/access_token=sk.eyJ1IjoidGFoYW5pIiwiYSI6ImkzZzlHaE0ifQ.oUpKTblIElF_hDsUKN6Lqw', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+}).addTo(map);
+//
+var marker = L.marker([51.5, -0.09]).addTo(map);
+//
+var circle = L.circle([51.508, -0.11], 500, {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
+//
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+//
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+//
+var popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+/*
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+map.on('click', onMapClick);
+*/
+var popup = L.popup();
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+map.on('click', onMapClick);
