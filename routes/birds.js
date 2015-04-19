48452671ce79dcde3a7c894faf5a413c2d679bcf
Jake Smith <jakehockey10@gmail.com>
2015-04-19 13:45:35 -0600

    refactor the birds routes

--- routes/birds.js
+++ routes/birds.js
@@ -5,81 +5,67 @@ var request = require('request');
 var Qs = require('qs');
 var jade = require('jade');
 
-var nodemw = require('nodemw');
-var client = new nodemw({
-    server: 'en.wikipedia.org', // host name of MediaWiki-powered site
-    path: '/w',                 // path to api.php script
-    debug: false                // is more verbose when set to true
-});
+//var nodemw = require('nodemw');
+//var client = new nodemw({
+//    server: 'en.wikipedia.org', // host name of MediaWiki-powered site
+//    path: '/w',                 // path to api.php script
+//    debug: false                // is more verbose when set to true
+//});
 
 var express = require('express');
 var router = express.Router();
 
-/* Returns recent nearby observations. */
-router.get('/data/obs/geo/recent', function(req, res, next) {
-    var url = 'http://ebird.org/ws1.1/data/obs/geo/recent?' +
-        Qs.stringify(req.query) + '&fmt=json';
+/**
+ * URL pieces
+ * @type {string}
+ */
+const eBirdDataServices = 'http://ebird.org/ws1.1/data';
+const recentNearbyObservations = '/obs/geo/recent?';
+const recentNearbyObservationsOfASpecies = '/nearest/geo_spp/recent?';
+const format = '&fmt=json';
+
+/**
+ * Make request to URL and send the response.
+ * @param url
+ * @param res
+ */
+function makeRequestToEBirdAPI(url, res) {
     request(url, function (err, resp, body) {
         var data = {
             err: err,
             resp: resp,
             body: body
         };
-        //var bodyParsed = JSON.parse(data.body);
-        //var imageArray = [];
-
-        //function getBatch(start) {
-        //    client.getImages(start, function(err, data, next) {
-        //        imageArray = imageArray.concat(data);
-        //        if (next) {
-        //            console.log('Getting next batch (starting from ' + next + ')...');
-        //            getBatch(next);
-        //        }
-        //        else {
-        //            console.log(JSON.stringify(imageArray, null, '\t'));
-        //            console.log('Image count: ' + imageArray.length);
-        //        }
-        //    });
-        //}
-        //
-        //getBatch(0);
-        //var images = [];
-        //for (var i = 0; i < 1; i++) {
-        //    console.log(bodyParsed[i].comName);
-        //
-        //    client.getArticle(bodyParsed[i].comName, function (err, articleData) {
-        //        if (err) { console.error(err) }
-        //        console.log('article: ');
-        //        console.log(typeof articleData);
-                //client.getImagesFromArticle(bodyParsed[i].comName, function (err, imageData) {
-                //    if (err) { console.error(err) }
-                //    console.log('images: ');
-                //    console.log(imageData);
-                //    imageData.forEach(function (image) {
-                //        images.push(image);
-                //    });
-                //    data.images = images;
         res.send(data);
-                //})
-            //});
-        //}
     })
+}
+
+/**
+ * Construct the URL that will be used to make a request to the eBird API.
+ * @param eBirdService (data, reference, etc.)
+ * @param path (path within that service)
+ * @param req (query parameters)
+ * @param format (json, xml, etc.)
+ * @returns {*}
+ */
+function constructURL(eBirdService, path, query, format) {
+    return eBirdService + path + Qs.stringify(query) + format;
+}
+
+/**
+ * Express route for "Recent Nearby Observations" endpoint of the eBird API.
+ */
+router.get('/data/obs/geo/recent', function(req, res, next) {
+    var url = constructURL(eBirdDataServices, recentNearbyObservations, req.query, format);
+    makeRequestToEBirdAPI(url, res);
 });
 
 /**
  * Express route for "Nearest Locations With Observations Of A Species" endpoint of the eBird API.
  */
 router.get('/data/nearest/geo_spp/recent', function(req, res, next) {
-    var url = 'http://ebird.org/ws1.1/data/nearest/geo_spp/recent?' +
-        Qs.stringify(req.query) + '&fmt=json';
-    request(url, function (err, resp, body) {
-        var data = {
-            err: err,
-            resp: resp,
-            body: body
-        };
-        res.send(data);
-    })
+    var url = constructURL(eBirdDataServices, recentNearbyObservationsOfASpecies, req.query, format);
+    makeRequestToEBirdAPI(url, res);
 });
 
 module.exports = router;
