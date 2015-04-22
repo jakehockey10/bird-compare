9fa488d6e94cc7c631fa82fcce580a0a4f2a9b34
Your Name <Tahani@Tahani-Almanies-MacBook-Pro.local>
2015-04-22 00:09:30 -0600

    combine states info in one json file and git rid of the two for-loops in app.js

--- app.js
+++ app.js
@@ -11,40 +11,8 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'jade');
 
-// TODO: we should see what the easiest way to hide this from the app.js file (or just make this happen without the double for loops...)
-/**
- * This combines our state data from two separate files into one array
- * so that we can persist the lat and lng values on the dom itself and
- * refer to the data-lat and data-lng attributes of the <select>'s <option>s.
- * This provides a way to easily send the lat and lng values along with
- * the call to the Leaflet map :)
- * @returns {Array}
- */
-function combineStatesJSONWithStatesJeoJSON() {
-    var statesWithGeo = [];
-    for (var i = 0; i < statesJeo.length; i++) {
-        var stateJeo = statesJeo[i];
-        var abbreviation = stateJeo['FIELD1'];
-        var lat = parseFloat(stateJeo['FIELD2']);
-        var lng = parseFloat(stateJeo['FIELD3']);
-        for (var j = 0; j < states.length; j++) {
-            var state = states[j];
-            if (state.abbreviation == abbreviation) {
-                statesWithGeo.push({
-                    name: state.name,
-                    abbreviation: state.abbreviation,
-                    location: {lat: lat, lng: lng}
-                });
-                break;
-            }
-        }
-    }
-    return statesWithGeo;
-}
-
 app.get('/', function (req, res) {
-    var statesWithGeo = combineStatesJSONWithStatesJeoJSON();
-    res.render('index.jade', {states: statesWithGeo, species: species})
+    res.render('index.jade', {states: states, species: species})
 });
 
 app.use('/birds', birds);
